88f6e9df031f5fcc73c0d35cec9ad29059003f30
clashingera <142937451+clashingera@users.noreply.github.com>
2024-02-07 15:02:55 +0530

    Update script.js

--- login-page/script.js
+++ login-page/script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function () {
         // For demonstration purposes, you can add a condition to simulate a successful login
         // You should replace this condition with your actual login logic
         if (username === "A" && password === "A") {
-            window.location.href = "home.html";
+            window.location.href = "dashboard/home.html";
             // window.location.href = "https://clashingera.github.io/EXPERI/dashboard/home.html";
             // window.location.href = "dashboard/home.html";
         } else {
